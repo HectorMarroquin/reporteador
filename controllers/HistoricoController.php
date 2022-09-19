a5d786ed74a3168a874f91fb5ca112046ad1b092
jesusasc <jesusasilva958@gmail.com>
2022-09-19 15:45:31 -0500

    se manda fechas por ajax a desglose en controllers

--- controllers/HistoricoController.php
+++ controllers/HistoricoController.php
@@ -1,12 +1,45 @@
 <?php
+require_once 'models/BitacoraValidacion.php';
+require_once 'models/VentasPospago.php';
+require_once 'controllers/HomeController.php';
 
 class HistoricoController 
 {
 	public function index(){
+		$fecha_i = $_POST['date1'];
+		$fecha_f = $_POST['date2'];
+
+		//var_dump($fecha_i);
+
 
 		Utils::checkSession();
-		require_once 'views/historico/historico.php';
+
+		require 'views/historico/historico.php';
 	}
+
+
+	public function desglose(){
+
+		$fecha_i = $_POST['date1'];
+		$fecha_f = $_POST['date2'];
+
+		$centrosPre = new BitacoraValidacion();
+		$centrosPrepagos = $centrosPre->getAll($fecha_i,$fecha_f);
+		
+		$datosObtenidos = new HomeController();
+		$reporteCentro = $datosObtenidos->getDesgloseCentros($centrosPrepagos,$fecha_i,$fecha_f);
+
+		var_dump($reporteCentro); exit();
+
+			echo json_encode($reporteCentro);
+			exit();
+
+	}
+
+
 }
+
+
+
 
 ?>
