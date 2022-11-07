1e6c9ddaa0d36d9e742fbe19dc36cc0eff40e07e
jesusasc <jesusasilva958@gmail.com>
2022-11-07 14:20:36 -0600

    Se crea nueva funcion para la consulta Reporte Sector. Se crea funcion para crear el archivo scv con phpSpreadsheet

--- controllers/HistoricoController.php
+++ controllers/HistoricoController.php
@@ -5,12 +5,19 @@ require_once 'models/VentasPospago.php';
 require_once 'controllers/HomeController.php';
 require_once 'models/UsuarioCliente.php';
 require_once 'helpers/utils.php';
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
 
 class HistoricoController
 {
 	public function index()
 	{
 		Utils::checkSession();
+
+		
+
 
 		require 'views/historico/historico.php';
 	}
@@ -64,24 +71,51 @@ class HistoricoController
 		$coaches = $coaches->getCoaches();
 
 
-		$desgloseHoraCoach = $this->desgloseHoraCoach($coaches, $fecha_i, $fecha_f);
+		$desgloseHoraCoach = $this->desgloseHoraCoach($fecha_i, $fecha_f, $coaches);
 
 		echo json_encode($desgloseHoraCoach);
 	}
 
-	public function desgloseHoraCoach($getCoach, $fecha_i, $fecha_f)
+	public function desgloseHoraCoach($fecha_i, $fecha_f, $getCoach)
 	{
-		
 		$ventashoracoaches = new BitacoraValidacion();
 		
 		while ($coach = $getCoach->fetch_object()) {
-			$coachId = $coach->Id;
-			
-			$getCoachHora = $ventashoracoaches->gethoraventaCoach($fecha_i, $fecha_f, $coachId);
-			$horasCoach[$coach->Nombre] = Utils::segmentaHoras($getCoachHora);
-		}
-		// $gethoraventaCoach
+			//$coachId = $coach->Id;
+			$getCoachHora = $ventashoracoaches->gethoraventaCoach($fecha_i, $fecha_f, $coach->Id);
+				$horasCoach[$coach->Nombre] = Utils::segmentaHoras($getCoachHora);
+			}
 		return $horasCoach;
 	}
 
+	public function desgloseSector()
+	{
+		$fecha_i = $_POST['date1'];
+		$fecha_f = $_POST['date2'];
+
+		$sector = new UsuarioCliente();
+		$getSectores = $sector->getSectores($fecha_i, $fecha_f);
+		$desglosector = new HomeController();
+		$getdesgloseSector = $desglosector->getDesgloSector($getSectores, $fecha_i, $fecha_f);
+		echo json_encode($getdesgloseSector);
+	}
+
+	public function generarCSV(){
+
+		$fecha_i = $_POST['date1'];
+		$fecha_f = $_POST['date2'];
+		$ventasCoach = new BitacoraValidacion();
+		$ventaCoach = $ventasCoach->getVentasCoach($fecha_i, $fecha_f);
+		$reporteCoach = HomeController::getDesgloseCoaches($ventaCoach, $fecha_i, $fecha_f);
+		
+		$header = [array("Coach", "Pagos", "Migradas", "Pos/Base", "Total", "Asistencia", "Factor", "Horas Conexion", "Talk", "SPH")];
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		$sheet->fromArray($header, NULL, 'A1')
+		->fromArray($reporteCoach, NULL, 'A2');     
+
+		$ruta = __DIR__ .'/save/ReporteCoaches.csv';
+		$writer = new Csv($spreadsheet);
+		$writer->save($ruta);
+	}
 }
